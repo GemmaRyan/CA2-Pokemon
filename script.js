9ab39c792ef152dac2cb645ac530c85f194d56b0
GemmaRyan <s00250864@atu.ie>
2024-12-02 19:54:46 +0000

    Changed API due to problems

--- script.js
+++ script.js
@@ -1,63 +1,42 @@
-let poke = 1;
-
- const url = `https://pokemon-hub.p.rapidapi.com/${poke}`;
-
-const options = {
-
-    method: 'GET',
-
-    headers: {
-
-        'x-rapidapi-key': '961bad8419msh0500e37e751bc94p169a5cjsnd301b51554b7',
-
-        'x-rapidapi-host': 'pokemon-hub.p.rapidapi.com'
-
-    }
-
-};
-
-
-
-
-async function CallingJson()
-
-{
-
-    try {
-
-        const response = await fetch(url, options);
-
-        const result = await response.json();
-
-       
-
- 
-
-        console.log(result.data[0].name);
-
-        const showMarvel = document.getElementById('content')
-
- 
-
-            const info = `
-
-           
-
-            <div>${result.data[0].name}</div>
-
-   
-
-            `;
-
-            showMarvel.innerHTML += info;
-
-
-
-
-    } catch (error) {
-
-        console.error(error);
-    }
+const baseUrl = 'https://pokeapi.co/api/v2/pokemon/'
+try {
+    fetch(baseUrl)
+    .then(response => {
+        const responseJson = response.json()
+        return responseJson
+    })
+    .then(data => {
+        const pokemons = data.results
+        pokemons.forEach(pokemon => {
+        document.getElementById('pokemons')
+            .insertAdjacentHTML('beforeend', `<li onclick='detail("${pokemon.url}")'>${pokemon.name}</li>`)
+        })
+    })
+    .catch(error => {
+        console.error(error)
+    })
+} catch (error) {
+    console.error(error)
 }
 
-CallingJson();
+const detail = (url) => {
+    try {
+    fetch(url)
+        .then(response => response.json())
+        .then(pokemon => {
+        document.getElementById('detail').innerHTML = ''
+        document.getElementById('detail')
+            .insertAdjacentHTML('beforeend', 
+            `
+                <h1>${pokemon.name}</h1>
+                <img src="${pokemon.sprites.front_default}" />
+            `
+            )
+        })
+        .catch(error => {
+        console.error(error)
+        })
+    } catch (error) {
+    console.error(error)
+    }
+}
