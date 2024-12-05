1ea1487baaec607e3e4db5780f33f31575542d73
GemmaRyan <s00250864@atu.ie>
2024-12-05 16:35:17 +0000

    Second push due to error

--- script.js
+++ script.js
@@ -1,42 +1,48 @@
-    const baseUrl = 'https://pokeapi.co/api/v2/pokemon/'
+const baseUrl = 'https://pokeapi.co/api/v2/pokemon/'
+try {
+    fetch(baseUrl)
+    .then(response => {
+        const responseJson = response.json()
+        return responseJson
+    })
+    .then(data => {
+        const pokemons = data.results
+        pokemons.forEach(pokemon => {
+        document.getElementById('pokemons')
+            .insertAdjacentHTML('beforeend', `<li onclick='detail("${pokemon.url}")'>${pokemon.name}</li>`)     //this Lets the url insde the json to open and become accessible 
+        }) 
+    })
+    .catch(error => {
+        console.error(error)
+    })
+} catch (error) {
+    console.error(error)
+}
+
+const detail = (url) => {
     try {
-        fetch(baseUrl)
-        .then(response => {
-            const responseJson = response.json()
-            return responseJson
-        })
-        .then(data => {
-            const pokemons = data.results
-            pokemons.forEach(pokemon => {
-            document.getElementById('pokemons')
-                .insertAdjacentHTML('beforeend', `<li onclick='detail("${pokemon.url}")'>${pokemon.name}</li>`)     //this Lets the url insde the json to open and become accessible 
-            }) 
+    fetch(url)
+    
+        .then(response => response.json())
+        .then(pokemon => {
+        document.getElementById('detail').innerHTML = ''
+        document.getElementById('detail')
+            .insertAdjacentHTML('beforeend', 
+            `
+                <h1>${pokemon.name}</h1>
+                <img src="${pokemon.sprites.front_default}" />
+                <p>Type : ${pokemon.types[0].type.name}</p>
+
+                <img id = shiny src="${pokemon.sprites.front_shiny}" />
+
+            `
+            )
+            console.log(pokemon.types)
         })
         .catch(error => {
-            console.error(error)
+        console.error(error)
         })
     } catch (error) {
-        console.error(error)
+    console.error(error)
     }
-
-    const detail = (url) => {
-        try {
-        fetch(url)
-            .then(response => response.json())
-            .then(pokemon => {
-            document.getElementById('detail').innerHTML = ''
-            document.getElementById('detail')
-                .insertAdjacentHTML('beforeend', 
-                `
-                    <h1>${pokemon.name}</h1>
-                    <img src="${pokemon.sprites.front_default}" />
-                `
-                )
-            })
-            .catch(error => {
-            console.error(error)
-            })
-        } catch (error) {
-        console.error(error)
-        }
-    }
+}
